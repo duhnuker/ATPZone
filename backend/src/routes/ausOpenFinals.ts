ea72dadf289ae486b6942ab26663041964163418
duhnuker <dinukanh@gmail.com>
2025-05-25 23:02:36 +1000

    get womensSinglesFinals data

--- backend/src/routes/ausOpenFinals.ts
+++ backend/src/routes/ausOpenFinals.ts
@@ -4,20 +4,43 @@ import fs from 'fs';
 
 const router: Router = express.Router();
 
-router.get('/mensfinals', (req: Request, res: Response) => {
+router.get('/aomenssinglesfinals', (req: Request, res: Response) => {
     try {
-
         const ausOpenMensFinalsData: any = [];
 
         fs.createReadStream('../DataScraping/australian_open_men_singles_champions.csv')
             .pipe(csv())
             .on('data', (data) => ausOpenMensFinalsData.push(data))
             .on('end', () => {
-                console.log(ausOpenMensFinalsData);
+                res.json(ausOpenMensFinalsData);
+            })
+            .on('error', (error) => {
+                console.error('Error reading CSV:', error);
+                res.status(500).json({ error: "Error reading CSV file" });
             });
 
     } catch (error: unknown) {
         res.status(500).json({ error: "Error fetching aus open mens finals data" });
+    }
+});
+
+router.get('/aowomenssinglesfinals', (req: Request, res: Response) => {
+    try {
+        const ausOpenWomensFinalsData: any = [];
+
+        fs.createReadStream('../DataScraping/australian_open_women_singles_champions.csv')
+            .pipe(csv())
+            .on('data', (data) => ausOpenWomensFinalsData.push(data))
+            .on('end', () => {
+                res.json(ausOpenWomensFinalsData);
+            })
+            .on('error', (error) => {
+                console.error('Error reading CSV:', error);
+                res.status(500).json({ error: "Error reading CSV file" });
+            });
+
+    } catch (error: unknown) {
+        res.status(500).json({ error: "Error fetching aus open womens finals data" });
     }
 });
 
