949fc30ea20e8c60b2ef75c8dba633e390889536
duhnuker <dinukanh@gmail.com>
2025-05-26 20:36:17 +1000

    ao singles schema

---
+++ backend/db.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE ATPZone
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE aus_open_mens_singles (
+    id SERIAL PRIMARY KEY,
+    year INT NOT NULL,
+    champion_country VARCHAR(3) NOT NULL,
+    champion_name VARCHAR(100) NOT NULL,
+    runner_up_country VARCHAR(3) NOT NULL,
+    runner_up VARCHAR(100) NOT NULL,
+    score VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE aus_open_womens_singles (
+    id SERIAL PRIMARY KEY,
+    year INT NOT NULL,
+    champion_country VARCHAR(3) NOT NULL,
+    champion_name VARCHAR(100) NOT NULL,
+    runner_up_country VARCHAR(3) NOT NULL,
+    runner_up VARCHAR(100) NOT NULL,
+    score VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,33 @@
 import express, { Request, Response, Router } from 'express';
-import pkg from 'pg'
-const { Pool } = pkg;
+import { Pool } from 'pg';
 import cors from 'cors';
 import 'dotenv/config';
 import ausOpenFinals from '../src/routes/ausOpenFinals.js'
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDBNAME,
+    password: process.env.PGPASSWORD,
+    port: 5432
+});
+
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error("Database connection error:", err);
+    } else {
+        console.log('Database connection successful');
+    }
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", async(req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
     res.send("Hello World");
 });
 
