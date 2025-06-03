bee3081bf65c7e5070dd1b79fd02a51279169ba4
duhnuker <dinukanh@gmail.com>
2025-06-03 21:46:01 +1000

    doubles finals schema

--- backend/db.sql
+++ backend/db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE ATPZone
+CREATE DATABASE AOFever
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
@@ -7,8 +7,8 @@ CREATE TABLE aus_open_mens_singles (
     year INT NOT NULL,
     champion_country VARCHAR(3) NOT NULL,
     champion_name VARCHAR(100) NOT NULL,
-    runner_up_country VARCHAR(3) NOT NULL,
-    runner_up VARCHAR(100) NOT NULL,
+    runners_up_country VARCHAR(3) NOT NULL,
+    runners_up VARCHAR(100) NOT NULL,
     score VARCHAR(50) NOT NULL
 );
 
@@ -17,7 +17,27 @@ CREATE TABLE aus_open_womens_singles (
     year INT NOT NULL,
     champion_country VARCHAR(3) NOT NULL,
     champion_name VARCHAR(100) NOT NULL,
-    runner_up_country VARCHAR(3) NOT NULL,
-    runner_up VARCHAR(100) NOT NULL,
+    runners_up_country VARCHAR(3) NOT NULL,
+    runners_up VARCHAR(100) NOT NULL,
+    score VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE aus_open_mens_doubles (
+    id SERIAL PRIMARY KEY,
+    year INT NOT NULL,
+    champions_countries VARCHAR(10) NOT NULL,
+    champions_names VARCHAR(100) NOT NULL,
+    runners_up_countries VARCHAR(10) NOT NULL,
+    runners_up_names VARCHAR(100) NOT NULL,
+    score VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE aus_open_womens_doubles (
+    id SERIAL PRIMARY KEY,
+    year INT NOT NULL,
+    champions_countries VARCHAR(10) NOT NULL,
+    champions_names VARCHAR(100) NOT NULL,
+    runners_up_countries VARCHAR(10) NOT NULL,
+    runners_up_names VARCHAR(100) NOT NULL,
     score VARCHAR(50) NOT NULL
 );
